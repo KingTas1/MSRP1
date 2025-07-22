bdeced4cbe80f87b98ead09fd0a1894e75c4b869
KingTas1 <chertaslim15@gmail.com>
2025-07-22 15:45:43 +0200

    final header

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 const navbarToggleBtn = document.querySelector(".nav__toggle-button")
-const mainNav = document.querySelector(".main-nav")
+const mainNav = document.querySelector(".header-nav")
 
 navbarToggleBtn.addEventListener("click", toggleNavigation)
 
 function toggleNavigation() {
-    if(mainNav.classList.contains("main-nav--active")) {
+    if(mainNav.classList.contains("header-nav--active")) {
         navbarToggleBtn.querySelector("img").src = "./assets/logos/hamburger.svg"
     }
     else {
         navbarToggleBtn.querySelector("img").src = "./assets/logos/close.svg"
     }
-    mainNav.classList.toggle("main-nav--active")
+    mainNav.classList.toggle("header-nav--active")
 }
 
 const navLinks = document.querySelectorAll(".nav__link")
